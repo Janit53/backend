53169c019a49f0574d03fbf3fd6268d032d33d0d
Janit53 <epix8226@gmail.com>
2025-06-16 12:19:53 +0530

    utils added

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "16kb" }));  // helps accept json data
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))  // parses url
+app.use(express.static("public")); // storring publically
+app.use(cookieParser())
+
+export { app }
