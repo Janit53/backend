83fe6ca768d94965b257743a4bef8d816f12adfb
Janit53 <epix8226@gmail.com>
2025-07-17 00:19:27 +0530

    fix: correct typo in cover image upload route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -28,7 +28,7 @@ router.route("/change-password").post(verfifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verfifyJWT, getCurrentUser)
 router.route("/update-account").patch(verfifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verfifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verfifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verfifyJWT, upload.single("coverImage"), updateUserCoverImage)
 router.route("/c/:username").get(verfifyJWT, getUserChannelProfile)
 router.route("/watch-histroy").get(verfifyJWT, getWatchHistory)
 
